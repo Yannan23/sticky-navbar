db7470449b3882d3fe81eebc0271442c1ad3e978
Yannan23 <yannansun0723@gmail.com>
2025-01-09 12:12:05 +1100

    fix the input area transition issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inputElement = document.getElementById('search-input')
 const searchButton = document.querySelector('button')
-const search = document.getElementsByClassName('search')
+// const search = document.getElementsByClassName('search')
 
 searchButton.addEventListener('click', function (event) {
     inputElement.classList.toggle('active');
