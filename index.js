d4f731adb1354c810899e496067e68d7efba9526
Yannan23 <yannansun0723@gmail.com>
2025-01-25 11:11:21 +1100

    initialize the project

--- index.js
+++
@@ -1,8 +0,0 @@
-const inputElement = document.getElementById('search-input')
-const searchButton = document.querySelector('button')
-// const search = document.getElementsByClassName('search')
-
-searchButton.addEventListener('click', function (event) {
-    inputElement.classList.toggle('active');
-    // input.focus()
-})
